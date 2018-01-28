62b7b3630ecef4c6153f2e8fc14d169e6fee4936
han0110 <qwerzxcv0110@gmail.com>
2018-01-28 17:01:32 +0800

    :tada: Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const About = () => (
+  <div className={styles.wrapper}>
+    Here is Jason Jian&apos;s world
+  </div>
+);
+
+export default About;
